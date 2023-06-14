3f8173c45ab6ee23612c03cc588f777be1cacf3a
Darsing <vanessadjeumeni@yahoo.fr>
2023-06-14 22:26:23 +0200

    creazione

--- Identity_gestione/DB/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Identity_gestione.DB.Entities
-{
-    public class User : IdentityUser
-    {
-    }
-}
--- Identity_gestione/Models/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Identity_gestione.Models
-{
-    public class LoginModel
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }    
-    }
-}
