fbcefaa785c74668ea9cc73e165c05b8f63c75be
Darsing <vanessadjeumeni@yahoo.fr>
2023-06-15 16:08:02 +0200

    creazione di user e role ,edit di user e role,delete user e role..

---
+++ MVCFramework/Controllers/UserDetailsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCFramework.DB.Entities;
+using MVCFramework.DB;
+using MVCFramework.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVCFramework.Controllers
+{
+    public class UserDetailsController : Controller
+    {
+        public IActionResult Index([FromServices] UserDBContext dBContext, string userID)
+        {
+            User user = dBContext.Users.Where(u => u.Id == userID).Select(us => new User()
+            {
+                Id = us.Id,
+                Email = us.Email,
+                UserName = us.UserName,
+                UserRoles = (from r in dBContext.Roles
+                             join ur in dBContext.UserRoles.Where(ur => ur.UserId == userID) on r.Id equals ur.RoleId
+                             select new Role()
+                             {
+                                 IdentityRole = r,
+                                 RoleClaims = dBContext.RoleClaims.Where(rc => rc.RoleId == r.Id).ToList()
+                             }).ToList(),
+                UserClaims = dBContext.UserClaims.Where(uc => uc.UserId == userID).ToList()
+            }).FirstOrDefault();
+            return View(new UsersAndRolesViewModel()
+            {
+                Users = new List<User>() { user }
+            });
+            return View(user);
+        }
+    }
+}
--- MVCFramework/wwwroot/js/site.js
+++ MVCFramework/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function hideModal() {
+    document.getElementById("modal-header").innerText = "";
+    $(".modal-body").empty();
+    $(".modal-footer").empty();
+    document.getElementById("modal").style.display = "none";
+}
