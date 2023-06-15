77fd313546242b87f03f1a5c1b0008eab300b960
Darsing <vanessadjeumeni@yahoo.fr>
2023-06-15 21:09:46 +0200

    jh

--- MVCFramework/wwwroot/js/site.js
+++
@@ -1,6 +0,0 @@
-﻿function hideModal() {
-    document.getElementById("modal-header").innerText = "";
-    $(".modal-body").empty();
-    $(".modal-footer").empty();
-    document.getElementById("modal").style.display = "none";
-}
